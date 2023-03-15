920b3a910c4decc645753c7dbfaa32613e263d24
Mustafa KARAGÜLLE <mustafacelalettinkaragulle@gmail.com>
2023-03-15 16:45:37 +0300

    Revize

--- src/Http/Controllers/PhotoController.php
+++ src/Http/Controllers/PhotoController.php
@@ -10,8 +10,8 @@ use Image;
 class PhotoController extends BaseController
 {
   public function getPhoto(Request $request){
-    $photo = "{$request->name}{$request->ext}";
-    $cachePhoto = "{$request->name}_{$request->width}_{$request->height}{$request->ext}";
+    $photo = "{$request->name}.{$request->filetype}";
+    $cachePhoto = "{$request->name}_{$request->width}_{$request->height}.{$request->filetype}";
     $cache = config('image-resizer.cachePath')."/{$cachePhoto}";
     $original = config('image-resizer.originalPath')."/{$photo}";
     $default = config('image-resizer.originalPath')."/".config('image-resizer.defaultImg');
@@ -21,7 +21,7 @@ class PhotoController extends BaseController
     else if(Storage::exists($original))
     {
       $img = new ImageResizer();
-      if($img->open($photo, $request->width, $request->height, str_replace(".", "", $request->ext)) != false){
+      if($img->open($photo, $request->width, $request->height, $request->filetype) != false){
         $image = $original;
       } else {
         $image = $default;
@@ -30,6 +30,11 @@ class PhotoController extends BaseController
     else{
       $image = $default;
     }
-    return Image::make(Storage::get($image))->response();
+    return Image::make(Storage::get($image))
+      ->resize($request->width, $request->height, function ($constraint) {
+        $constraint->aspectRatio();
+        $constraint->upsize();
+      })
+      ->encode(str_replace('.', '', $request->ext))->response();
   }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use Karagulle\ImageResizer\Http\Controllers\PhotoController;
 
-Route::get('photo/{name}_{width}_{height}{ext}', [PhotoController::class, 'getPhoto'])->where(['name'=> '[a-zA-Z0-9.\-_\/]+', 'ext' => '\..+'])->name('get.photo');
+Route::get('photo/{name}_{filetype}_{width}_{height}{ext}', [PhotoController::class, 'getPhoto'])->where(['name'=> '[a-zA-Z0-9.\-_\/]+', 'ext' => '\..+'])->name('get.photo');
